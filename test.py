6ffbdf09735838d0393409785b3423861982a9f6
Mahnaz Sarker Meem <108950657+Mahnaz-Meem@users.noreply.github.com>
2024-02-09 15:24:49 -0500

    Update test by Meem.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+
+# %pip install -U leafmap
+# %pip install -U rasterstats geopandas
+dsm = 'https://open.gishub.org/data/elevation/dsm.tif'
+hag = 'https://open.gishub.org/data/elevation/hag.tif'
+buildings = 'https://open.gishub.org/data/elevation/buildings.geojson'
+m = leafmap.Map()
+m.add_cog_layer(dsm, name='DSM', palette='terrain')
+m.add_cog_layer(hag, name='Height Above Ground', palette='magma')
+m.add_geojson(buildings, layer_name='Buildings')
+m
