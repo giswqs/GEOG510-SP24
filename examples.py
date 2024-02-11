47f70db81b94a566ec9e2da413102a1c0da25631
Wanhee Kim <158336706+whkim15@users.noreply.github.com>
2024-02-11 18:38:36 -0500

    Making some changes (#10)

--- examples.py
+++ examples.py
@@ -1,1 +1,4 @@
-print("Howdy")
+print("Hello Dr.Wu!")
+print("I hope you had a great weekend!")
+
+print("Howdy")This commit does not belong to any branch on this repository, and may belong to a fork outside of the repository.
