1e759594cd1eaff33c77276443293443d83ffa5a
nathansturgill <147578367+nathansturgill@users.noreply.github.com>
2024-02-14 18:06:06 -0500

    Gif (#16)

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This a demo repo for GEOG 510 in Spring 2024
 
 This is just a line. 
+
+
+![9s8p](https://github.com/nathansturgill/GEOG510-SP24/assets/147578367/ee7b7568-aa1e-440f-bc63-bb84ecbd1c22)
--- examples.py
+++ examples.py
@@ -2,3 +2,4 @@ print("Hello Dr.Wu!")
 print("I hope you had a great weekend!")
 
 print("Howdy")This commit does not belong to any branch on this repository, and may belong to a fork outside of the repository.
+
