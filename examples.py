53dddc8d76cd00994a18a49a3af4a2aac426286d
nathan sturgill <nsturgi1@vols.utk.edu>
2024-02-14 17:26:42 -0500

    Created a message in example.py and placed a GIF image into the repository

--- examples.py
+++ examples.py
@@ -2,3 +2,6 @@ print("Hello Dr.Wu!")
 print("I hope you had a great weekend!")
 
 print("Howdy")This commit does not belong to any branch on this repository, and may belong to a fork outside of the repository.
+
+
+print("Dr. Wu please accept a gif to the repository!")
